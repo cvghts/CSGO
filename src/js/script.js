5a6885d75403862f609b5c1aa5786d0cc89b3c14
Zhenya Yarosh <zhenyayarosh16@gmail.com>
2025-08-22 13:01:54 +0300

    Added stages section

--- src/js/script.js
+++ src/js/script.js
@@ -38,7 +38,7 @@ try {
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
-          },
+        },
         navigation: {
           nextEl: '.button-next',
           prevEl: '.button-prev',
